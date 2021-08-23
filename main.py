111c1a3aced34d08c066697724e2045522f6382a
MikiS <73443709+RAV64@users.noreply.github.com>
2021-08-23 15:45:19 +0300

    get items too and cli and fixes :)

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import ssl
 import os
 
 import requests
+from time import sleep
 from requests.auth import HTTPBasicAuth
+from selenium.webdriver.common.keys import Keys
 
 from urllib3.exceptions import InsecureRequestWarning
 
@@ -19,7 +21,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 opts = Options()
-opts.headless = False
+opts.headless = True
 
 with open("rune_data.json", "r") as f:
     RUNE_DICT = json.load(f)
@@ -50,10 +52,13 @@ class runechanger:
         self.is_final_pick = None
         self.new_champ = (None, None)
         self.role_or_aram = None
+        self.skill_order = None
+        self.items = []
+        self.starting_items = []
         self.runes = {"primary_runes": None,
                       "secondary_runes": None,
                       "fragments": None}
-        print("LoL RuneChanger is active!")
+        print("LoL RuneChanger started up successfully.")
 
     async def listener(self):
         async with websockets.connect(self.url, ssl=self.ssl_context) as websocket:
@@ -70,20 +75,23 @@ class runechanger:
                         except:
                             continue
 
-                        if self.new_champ != (champ_name, self.assigned_role):
-                            self.new_champ = (champ_name, self.assigned_role)
+                        if self.new_champ != (champ_name, self.new_champ[1]):
+                            self.new_champ = (champ_name, self.new_champ[1])
                             self.__prepare_driver(champ_name.lower())
+                            new_champ_bool = True
 
-                        if self.is_final_pick:
+                        if self.is_final_pick and new_champ_bool:
                             self.__scrapeUGG()
                             self.__set_runes()
+                            self.__update_cli()
+                            new_champ_bool = False
 
     def __set_runes(self):
         base_url = f"{self.scheme}://127.0.0.1:{self.port}"
         auth_header = HTTPBasicAuth('riot', self.password)
         page_id = self.__get_page_id(base_url, auth_header)["id"]
         url = f"{base_url}/lol-perks/v1/pages/{str(page_id)}"
-        data = {"name": f"{self.new_champ[0]}, {self.role_or_aram}",
+        data = {"name": f"{self.new_champ[0]} @ {self.role_or_aram}",
                 "primaryStyleId": self.runes["primary_runes"][0],
                 "subStyleId": self.runes["secondary_runes"][0],
                 "selectedPerkIds": self.runes["primary_runes"][1:] +
@@ -112,29 +120,82 @@ class runechanger:
         else:
             self.assigned_role = "support"
 
+    @staticmethod
+    def __clean_role_items(role):
+        if role == "mid":
+            role = "middle"
+        elif role == "bot":
+            role = "adc"
+        return role
+
     def __prepare_driver(self, champ_name):
         if self.assigned_role:
             self.__clean_role(self.assigned_role.lower())
+            role = self.__clean_role_items(self.assigned_role)
             url = f"https://u.gg/lol/champions/{champ_name}/build?rank=diamond_2_plus&role={self.assigned_role}"
+            items_url = f"https://www.leagueofgraphs.com/champions/items/{champ_name}/{role}"
+
             self.role_or_aram = self.assigned_role
         else:
             url = f"https://u.gg/lol/champions/aram/{champ_name}-aram"
+            items_url = f"https://www.leagueofgraphs.com/champions/items/{champ_name}/diamond/aram"
             self.role_or_aram = "Aram"
 
+        driver.switch_to.window(driver.window_handles[0])
         driver.get(url)
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
             (By.XPATH, "//*[@id='content']/div/div[1]/div/div/div[5]/div/div[2]/div[1]/div[2]/div[1]/div[1]/div")))
 
+        if "https://static.u.gg/assets/ugg/icons/alert-yellow.svg" in driver.page_source:
+            print("Hep")
+            url = f"https://u.gg/lol/champions/{champ_name}/build?role={self.assigned_role}"
+            driver.get(url)
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+                (By.XPATH, "//*[@id='content']/div/div[1]/div/div/div[5]/div/div[2]/div[1]/div[2]/div[1]/div[1]/div")))
+
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(items_url)
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+            (By.XPATH, "//*[@id='topItemsTable']/tbody/tr[2]/td[1]/img")))
+
+    def __scrape_items(self):
+
+        self.items = []
+        self.starting_items = []
+
+        driver.switch_to.window(driver.window_handles[1])
+        soup = bs(driver.page_source, 'html.parser')
+        table = soup.find_all("table", {"class": "data_table sortable_table"})
+        trs = table[3].find_all("tr")
+        for i, tr in enumerate(trs):
+            if i == 0:
+                continue
+            elif i == 6:
+                break
+            self.items.append(tr.find("img")["alt"])
+
+        table = soup.find_all("table", {"class": "data_table itemStarters sortable_table"})
+        trs = table[0].find_all("tr")
+        for i, tr in enumerate(trs):
+            if i == 0:
+                continue
+            elif i == 4:
+                break
+            self.starting_items.append([x["alt"] for x in tr.find_all("img")])
+
     def __scrapeUGG(self):
+        driver.switch_to.window(driver.window_handles[0])
         page = driver.page_source
         soup = bs(page, 'html.parser')
-
         primary_tree = soup.find_all("div", {"class": "rune-tree_v2 primary-tree"})[0]
         secondary_tree = soup.find_all("div", {"class": "secondary-tree"})[0]
+        skill_list = soup.find_all("div", {"class": "skill-priority-path"})[0]
 
         primary_runes = self.__get_primary_runes(primary_tree)
         secondary_runes = self.__get_secondary_runes(secondary_tree)
         fragments = self.__get_fragments(secondary_tree)
+        self.__scrape_items()
+        self.__get_skill_order(skill_list)
 
         self.runes = {"primary_runes": primary_runes,
                       "secondary_runes": secondary_runes,
@@ -199,7 +260,28 @@ class runechanger:
         r = requests.get(url_champs).json()
         return r["name"], r["alias"]
 
+    def __get_skill_order(self, skill_list):
+        skills = skill_list.find_all("div", {"class": "skill-label bottom-center"})
+        self.skill_order = [skill.text for skill in skills]
+
+    def __update_cli(self):
+        os.system("clear")
+        print("-------------------------------------------")
+        print("Lol RuneChanger")
+        print(f"Champ: {self.new_champ[0]}")
+        print(f"Role: {self.role_or_aram}")
+        print(f"levelup: {' -> '.join(self.skill_order)}")
+
+        print(f"\nStarting items: \n")
+        for i, item in enumerate(self.starting_items):
+            print(f"\t{i + 1}. {', '.join(item)}")
+
+        print(f"\nMost common items: \n")
+        for i, item in enumerate(self.items):
+            print(f"\t{i+1}. {item}")
+
 
 rc = runechanger()
 with webdriver.Firefox(options=opts) as driver:
+    driver.execute_script("window.open('about:blank', 'tab2');")
     asyncio.get_event_loop().run_until_complete(rc.listener())
